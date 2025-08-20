262e2176f8b361c8983b8510404dd18e4969a2b4
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-08-20 20:39:45 +0530

    Update Corona.py

--- Corona.py
+++ Corona.py
@@ -1,5 +1,5 @@
 '''Every decimal number can be changed into its binary form. Suppose your computer has it’s own CoronaVirus, that eats binary digits from the right side of a number. Suppose a virus has 6 spikes, it will eat up 6 LSB binary digits in your numbers.
-You will have a bunch of numbers, and your machine will have a virus with n spikes, you have to calculate what will be the final situation of the final numbers.''
+You will have a bunch of numbers, and your machine will have a virus with n spikes, you have to calculate what will be the final situation of the final numbers.'''
 
 N = int(input())
 arr = list(map(int,input().split()))
