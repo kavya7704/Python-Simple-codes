b1823e4472cb1a4213420e291917164ec3d97cb6
kavya7704 <139637198+kavya7704@users.noreply.github.com>
2025-09-19 14:19:16 +0530

    Create Alphanumeric.py

---
+++ Alphanumeric.py
@@ -0,0 +1,25 @@
+
+def substring(s):
+    arr = []
+    curr = ""
+    for i in s:
+        if i.isdigit():
+            curr += i
+        else:
+            if curr:
+                arr.append(curr)
+                curr = ""
+            
+    if curr:
+        arr.append(curr)
+    print(arr)
+    count = 0
+    for i in range(0,len(arr) - 2):
+        if arr[i][-1] != arr[i+1][0]:
+            count += 1
+            
+    return count
+    
+s = "abc123cd45ef67gh8"
+
+print(substring(s))
